45add55e1434a00202009424d96bb7c00f779868
Tamas Nemes <t.nemes@axes4.com>
2022-09-07 14:49:46 +0200

    Add buttons for choosing answers

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,35 @@ void main(List<String> args) {
 }
 
 class MyApp extends StatelessWidget {
+  void onAnswerPressed() {
+    print("Answer chosen");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Text('Hello world!'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My first app"),
+        ),
+        body: Column(
+          children: [
+            const Text("The question!"),
+            ElevatedButton(
+              onPressed: onAnswerPressed,
+              child: const Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: onAnswerPressed,
+              child: const Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: onAnswerPressed,
+              child: const Text("Answer 3"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
